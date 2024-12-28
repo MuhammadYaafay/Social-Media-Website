b00013e4e1759fb933eafcc3160cd010bc5cd2f6
Yaafay <muhammad.yaffay@gmail.com>
2024-12-29 04:01:12 +0500

    Fix: remove submodule references and add folders

---
+++ SocialMediaAPI/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/post");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running on port 8800");
+});
---
+++ SocialMediaClient/src/pages/login/Login.jsx
@@ -0,0 +1,40 @@
+import "./login.css"
+import { useRef } from "react"    //ref used because of less rendering otherwise in usestate or other it would render on every character typed
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
+import { CircularProgress } from "@mui/material"
+
+export default function Login() {
+
+    const email = useRef();
+    const password = useRef();
+    const {user, isFetching, error, dispatch} = useContext(AuthContext);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall({ email: email.current.value, password: password.current.value }, dispatch);
+    }
+  return (
+    <div className="login">
+        <div className="loginWrapper">
+            <div className="loginLeft">
+                <h3 className="loginLogo">Social</h3>
+                <span className="loginDesc">
+                    Connect with friends and the world around you on Social.
+                </span>
+            </div>
+            <div className="loginRight">
+                <form className="loginBox" onSubmit={handleClick}>
+                    <input placeholder="Email" type="email" className="loginInput" required ref={email}/>
+                    <input placeholder="Password" type="password" className="loginInput" required minLength={6 } ref={password}/>
+                    <button className="loginButton" type="submit" disabled={isFetching}> {isFetching? <CircularProgress color="white" size="20px"/>: "login"}</button>
+                    <span className="loginForgot">Forgot Password?</span>
+                    <button className="loginRegisterButton">{isFetching? <CircularProgress color="white" size="20px"/>: "Create a new account"}</button>
+                </form>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
