0c0b55b66afaf88c41c37a6a3be4c258f4817473
Yaafay <muhammad.yaffay@gmail.com>
2025-01-01 23:44:47 +0500

    add Message and Conversation components with styling for the Messenger page

---
+++ SocialMediaClient/src/components/message/Message.jsx
@@ -0,0 +1,13 @@
+import React from "./message.css";
+
+export default function Message({own}) {
+  return (
+    <div className={own ? "message own":"message"}>
+      <div className="messageTop">
+        <img className="messageImg" src="assets/person/1.jpeg" alt="" />
+        <p className="messageText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, recusandae.</p>
+      </div>
+        <div className="messageBottom">1 hour ago</div>
+    </div>
+  );
+}
--- SocialMediaClient/src/pages/messenger/Messenger.jsx
+++ SocialMediaClient/src/pages/messenger/Messenger.jsx
@@ -1,5 +1,7 @@
 import "./messenger.css";
 import Topbar from "../../components/topbar/Topbar";
+import Conversation from "../../components/conversations/Conversation";
+import Message from "../../components/message/Message";
 
 const Messenger = () => {
   return (
@@ -7,15 +9,41 @@ const Messenger = () => {
       <Topbar />
       <div className="messenger">
         <div className="chatMenu">
-            <div className="chatMenuWrapper">
-                <input placeholder="Search for friends" className="chatMenuInput" />
-            </div>
+          <div className="chatMenuWrapper">
+            <input placeholder="Search for friends" className="chatMenuInput" />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+            <Conversation />
+          </div>
         </div>
         <div className="chatBox">
-            <div className="chatBoxWrapper"></div>
+          <div className="chatBoxWrapper">
+            <div className="chatBoxTop">
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+              <Message />
+
+            </div>
+            <div className="chatBoxBottom">
+              <textarea
+                className="chatMessageInput"
+                placeholder="write something..."
+              ></textarea>
+              <button className="chatSubmitButton">Send</button>
+            </div>
+          </div>
         </div>
         <div className="chatOnline">
-            <div className="chatOnlineWrapper"></div>
+          <div className="chatOnlineWrapper"></div>
         </div>
       </div>
     </>
