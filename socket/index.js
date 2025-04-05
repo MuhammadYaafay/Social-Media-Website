05ad47cd1d3ae62113d567725005e3763389ab2d
Yaafay <muhammad.yaffay@gmail.com>
2025-04-06 02:40:31 +0500

    fixed issues

--- socket/index.js
+++ socket/index.js
@@ -1,6 +1,6 @@
 const io = require("socket.io")(8900, {
   cors: {
-    origin: "http://localhost:3000", // Adjust according to your frontend's address
+    origin: "http://localhost:3000", 
   },
 });
 
